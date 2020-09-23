dbb8a7ffed8b5fac667021658eb3ab743ef0c72b
seth brown <sethjensenbrown@gmail.com>
2020-09-23 15:08:38 -0700

    mongoose update

--- server.js
+++ server.js
@@ -274,7 +274,7 @@ let server;
 function runServer(databaseUrl, port=PORT) {
 
   return new Promise((resolve, reject) => {
-    mongoose.connect(databaseUrl || DATABASE_URL, {useMongoClient: true} ,err => {
+	  mongoose.connect(databaseUrl || DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true } ,err => {
       if (err) {
         return reject(err);
       }
